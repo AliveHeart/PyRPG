f73e9303f8563e4237f4fa162502e02bf46e27a1
AliveHeart <mirarafathali7@gmail.com>
2026-07-02 13:46:50 +0530

    add player class

---
+++ player.py
@@ -0,0 +1,11 @@
+
+class Player :
+    def __init__(self):
+
+        self.health = 100
+        self.max_health = 100
+        self.luck = 0
+        self.current_location = "Town"
+        self.str = 1
+        self.spd = 1
+        self.endur = 1
