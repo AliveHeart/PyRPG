955ef636ddfb4713a7fb83dbe0ea81ececdb3ad6
AliveHeart <mirarafathali7@gmail.com>
2026-07-02 20:28:54 +0530

    Update player class

--- player.py
+++ player.py
@@ -9,3 +9,9 @@ class Player :
         self.str = 1
         self.spd = 1
         self.endur = 1
+        self.money = 100
+        self.inventory = []
+
+        self.helmet = 0
+        self.chestplate = 0
+        self.leggings = 0
