bd529394c86705ac873acf3c8b34673e08a345ab
AliveHeart <mirarafathali7@gmail.com>
2026-07-03 12:56:35 +0530

    Add look for entities

--- commands.py
+++ commands.py
@@ -21,7 +21,25 @@ def execute(game, string):
             else:
                 path_names += "" + area_name + ". "
 
+        entity_Names = ""
+        entity_no = len(current_location.entities)
+        for index, entity_name in enumerate(current_location.entities):
+            if entity_no > 1:
+                if (entity_no - 1) == index:
+                    entity_Names += "and " + entity_name + ". "
+                elif (entity_no - 2) == index:
+                    entity_Names += "" + entity_name + " "
+                else:
+                    entity_Names += "" + entity_name + ", "
+            else:
+                entity_Names += "" + entity_name + ". "
+
         renderer.render("You look around. " + current_location.description + " You see " + str(path_no) + " paths to " + path_names)
+        renderer.render("You spot " + entity_Names)
     elif action == "go":
         if arg[0] in current_location.connections:
-            game.player.current_location = arg[0]   
+            game.player.current_location = arg[0]
+
+            renderer.render("You reach the " + arg[0] + ". ")
+        else:
+            renderer.render("You can't find a way to " + arg[0] + ". ")
