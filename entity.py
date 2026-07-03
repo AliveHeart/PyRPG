9b984482a786ae1013c46f540cf09261463d8478
AliveHeart <mirarafathali7@gmail.com>
2026-07-03 12:56:55 +0530

    Update entities

--- entity.py
+++ entity.py
@@ -1,11 +1,15 @@
 
 class Entity :
-    def __init__(self, name, type, hp, hostile):
+    def __init__(self, name, type, hp, hostile, str, spd, exp):
 
         self.name = name
         self.health = hp
         self.max_health = hp
         self.hostile = hostile
+
+        self.str = str
+        self.spd = spd
+        self.exp = exp
 
         self.type = type
 
