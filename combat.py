9cfea2cdc91d0d72fc764b50e53f4aa891ee1d6f
AliveHeart <mirarafathali7@gmail.com>
2026-07-10 20:31:19 +0530

    Add descision tree for enemy AI

--- combat.py
+++ combat.py
@@ -1,4 +1,48 @@
-import random
+import random, randomer
+
+def chance_roll(enemy_atr, plr_atr):
+    ratio = enemy_atr / plr_atr
+    chance = min(1.0, 0.1 + 0.6 * (ratio / (1 + ratio))) * 100
+    roll = random.uniform(0, 100)
+
+    return roll < chance
+
+def enemy_attack(enemy, player):
+    plr_speed = player.spd * player.weapon_spd
+    
+    if chance_roll(enemy.spd, plr_speed):
+        return ("You dodge the " + enemy.name + " attack!")
+    else:
+        player.health -= enemy.str
+        return ("The " + enemy.name + " attacks you for " + str(enemy.str) + " damage!")
+    
+def enemy_descision(enemy, player):
+    attack_weightage = 80 + enemy.arrogance
+    defend_weightage = 20 + enemy.defensive
+    surrender_weightage = 10 + enemy.cowardness 
+    if enemy.health <= (enemy.max_health * 0.4) and enemy.health > (enemy.max_health * 0.2):
+        defend_weightage += 20
+        attack_weightage -= 20
+    elif enemy.health <= (enemy.max_health * 0.2):
+        defend_weightage += 20
+        attack_weightage -= 40
+        surrender_weightage += 20
+
+    if player.health < (player.max_health * 0.3):
+        attack_weightage += 30
+        defend_weightage -= 15
+        surrender_weightage -= 15
+
+    attack_weightage = max(0, attack_weightage)
+    defend_weightage = max(0, defend_weightage)
+    surrender_weightage = max(0, surrender_weightage)
+
+    weightage_list = {"attack": attack_weightage, "defend": defend_weightage, "surrender": surrender_weightage}
+    descision = randomer(weightage_list)
+    
+    if descision == "attack" :
+        return enemy_attack(enemy, player)
+
 
 def attack(game):
     enemy = game.world.entities[game.player.enemy]
@@ -8,15 +52,16 @@ def attack(game):
 
     enemy_spd = enemy.spd
 
-    ratio = enemy_spd / plr_speed
-    dodge_chance = min(1.0, 0.1 + 0.9 * (ratio / (1 + ratio))) * 100
-    roll = random.uniform(0, 100)
-
-    if roll < dodge_chance:
-        return ("The " + enemy.name + " dodges your attack!")
+    plr_feedback = " "
+    if chance_roll(enemy_spd, plr_speed):
+        plr_feedback = ("The " + enemy.name + " dodges your attack!")
     else:
         enemy.health -= plr_damage
-        return ("You hit " + enemy.name + " and deal " + str(plr_damage) + " damage!")
+        plr_feedback = ("You hit the " + enemy.name + " and deal " + str(plr_damage) + " damage!")
+    
+    enemy_feedback = enemy_descision(enemy, game.player)
+
+    return [plr_feedback, enemy_feedback]
 
 
     
