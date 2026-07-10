5d2d2532c9839b87f27475958c8f96436fb04bd5
AliveHeart <mirarafathali7@gmail.com>
2026-07-10 19:46:01 +0530

    Add attack

--- combat.py
+++ combat.py
@@ -1,4 +1,4 @@
-
+import random
 
 def attack(game):
     enemy = game.world.entities[game.player.enemy]
@@ -7,3 +7,16 @@ def attack(game):
     plr_speed = game.player.spd * game.player.weapon_spd
 
     enemy_spd = enemy.spd
+
+    ratio = enemy_spd / plr_speed
+    dodge_chance = min(1.0, 0.1 + 0.9 * (ratio / (1 + ratio))) * 100
+    roll = random.uniform(0, 100)
+
+    if roll < dodge_chance:
+        return ("The " + enemy.name + " dodges your attack!")
+    else:
+        enemy.health -= plr_damage
+        return ("You hit " + enemy.name + " and deal " + str(plr_damage) + " damage!")
+
+
+    
