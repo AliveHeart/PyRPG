9f721249be75ae95b620e8494fab23c8d2ff0855
AliveHeart <mirarafathali7@gmail.com>
2026-07-10 19:46:20 +0530

    Add In combat and attack command support

--- commands.py
+++ commands.py
@@ -1,4 +1,4 @@
-import renderer
+import renderer, combat
 
 def execute(game, string):
     action = string.split()[0]
@@ -7,39 +7,57 @@ def execute(game, string):
 
     current_location = game.world.areas[game.player.current_location]
 
-    if action == "look":
-        path_names = ""
-        path_no = len(current_location.connections)
-        for index, area_name in enumerate(current_location.connections):
-            if path_no > 1:
-                if (path_no - 1) == index:
-                    path_names += "and " + area_name + ". "
-                elif (path_no - 2) == index:
-                    path_names += "" + area_name + " "
+    if game.player.in_combat == False:
+        if action == "look":
+            path_names = ""
+            path_no = len(current_location.connections)
+            for index, area_name in enumerate(current_location.connections):
+                if path_no > 1:
+                    if (path_no - 1) == index:
+                        path_names += "and " + area_name + ". "
+                    elif (path_no - 2) == index:
+                        path_names += "" + area_name + " "
+                    else:
+                        path_names += "" + area_name + ", "
                 else:
-                    path_names += "" + area_name + ", "
-            else:
-                path_names += "" + area_name + ". "
+                    path_names += "" + area_name + ". "
 
-        entity_Names = ""
-        entity_no = len(current_location.entities)
-        for index, entity_name in enumerate(current_location.entities):
-            if entity_no > 1:
-                if (entity_no - 1) == index:
-                    entity_Names += "and " + entity_name + ". "
-                elif (entity_no - 2) == index:
-                    entity_Names += "" + entity_name + " "
+            entity_Names = ""
+            entity_no = len(current_location.entities)
+            for index, entity_name in enumerate(current_location.entities):
+                if entity_no > 1:
+                    if (entity_no - 1) == index:
+                        entity_Names += "and " + entity_name + ". "
+                    elif (entity_no - 2) == index:
+                        entity_Names += "" + entity_name + " "
+                    else:
+                        entity_Names += "" + entity_name + ", "
                 else:
-                    entity_Names += "" + entity_name + ", "
+                    entity_Names += "" + entity_name + ". "
+
+            renderer.render("You look around. " + current_location.description + " You see " + str(path_no) + " paths to " + path_names)
+            renderer.render("You spot " + entity_Names)
+        elif action == "go":
+            if arg[0] in current_location.connections:
+                game.player.current_location = arg[0]
+
+                renderer.render("You reach the " + arg[0] + ". ")
             else:
-                entity_Names += "" + entity_name + ". "
+                renderer.render("You can't find a way to " + arg[0] + ". ")
+        elif action == "fight":
+            if arg[0] in current_location.entities:
+                renderer.render("You deicde to fight " + arg[0])
+                game.player.in_combat = True
+                game.player.enemy = arg[0]
 
-        renderer.render("You look around. " + current_location.description + " You see " + str(path_no) + " paths to " + path_names)
-        renderer.render("You spot " + entity_Names)
-    elif action == "go":
-        if arg[0] in current_location.connections:
-            game.player.current_location = arg[0]
+                renderer.render("You are in combat with a " + game.player.enemy + ". ")
+                renderer.render("Actions : -> attack -> defend -> run -> surrender")
+    else:
+        if action == "attack":
+            condition = combat.attack(game)
 
-            renderer.render("You reach the " + arg[0] + ". ")
-        else:
-            renderer.render("You can't find a way to " + arg[0] + ". ")
+            renderer.render(condition)
+
+
+
+
