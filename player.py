3344601951b51d15d4c1dce6ddc8729c47e8d07c
AliveHeart <mirarafathali7@gmail.com>
2026-07-10 19:46:34 +0530

    Add more attributes

--- player.py
+++ player.py
@@ -5,12 +5,23 @@ class Player :
         self.health = 100
         self.max_health = 100
         self.luck = 0
+
         self.current_location = "Town"
+
         self.str = 1
-        self.spd = 1
+        self.spd = 3
         self.endur = 1
         self.money = 100
+
+        self.weapon = "fist"
+        self.weapon_dmg = 1
+        self.weapon_spd = 1
+
         self.inventory = []
+
+        self.honor = 0
+        self.in_combat = False
+        self.enemy = "air"
 
         self.helmet = 0
         self.chestplate = 0
