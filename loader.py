2ddc14b495ee56ea8865548c6dbbd6e1326525a0
AliveHeart <mirarafathali7@gmail.com>
2026-07-26 11:51:39 +0530

    Add json file loading and saving

--- loader.py
+++ loader.py
@@ -1,5 +1,12 @@
 import json
 
+saves_path = "data/save_slots/"
+
 def load_json(path):
-    with open(path) as f:
+    with open(path, "r") as f:
         return json.load(f)
+    
+def save_json(slot, data):
+    with open(saves_path + str(slot) + ".json", "w") as f:
+        json.dump(data, f, indent=4)
+        return True
