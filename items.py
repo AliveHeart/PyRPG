a318ce63ea9d9826198e4166867da4f7345b7c0b
AliveHeart <mirarafathali7@gmail.com>
2026-07-27 19:28:04 +0530

    Add item count

--- items.py
+++ items.py
@@ -5,10 +5,11 @@ items_path =  data_path + "items.json"
 
 def get_Item(id):
     item_ids = id.split()
+    u_id = " ".join(item_ids[:-1])
 
     if item_ids[0] == "01":
         items = loader.load_json(items_path)
-        if id in items:
-            return items[id]["name"]
+        if str(u_id) in items:
+            return items[u_id]["name"], item_ids[-1]
 
     return False
