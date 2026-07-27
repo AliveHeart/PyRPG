dedc29d7ae3411ccb4b753c0e17f52a7d0990406
AliveHeart <mirarafathali7@gmail.com>
2026-07-27 19:35:22 +0530

    Fix structure

--- items.py
+++ items.py
@@ -3,13 +3,13 @@ import loader
 data_path = "data/"
 items_path =  data_path + "items.json"
 
-def get_Item(id):
+def get(id):
     item_ids = id.split()
     u_id = " ".join(item_ids[:-1])
 
     if item_ids[0] == "01":
         items = loader.load_json(items_path)
         if str(u_id) in items:
-            return items[u_id]["name"], item_ids[-1]
+            return [items[u_id], str(int(item_ids[-1]))]
 
     return False
