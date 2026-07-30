98bc096281d316e9b82d80ab9778484919bd09c4
AliveHeart <mirarafathali7@gmail.com>
2026-07-30 16:28:02 +0530

    Add armour view in inventory

--- items.py
+++ items.py
@@ -1,15 +1,24 @@
 import loader
 
 data_path = "data/"
-items_path =  data_path + "items.json"
 
 def get(id):
     item_ids = id.split()
     u_id = " ".join(item_ids[:-1])
 
-    if item_ids[0] == "01":
-        items = loader.load_json(items_path)
+    if item_ids[0] == "1":
+        items = loader.load_json(data_path + "items.json")
         if str(u_id) in items:
             return [items[u_id], str(int(item_ids[-1]))]
+    else:
+        armor = loader.load_json(data_path + "armour.json")
+        material = loader.load_json(data_path + "material.json")
+        adjective = loader.load_json(data_path + "adjective-armour.json")
+        if item_ids[0] == "3":
+            adjective = loader.load_json(data_path + "adjective-weapon.json")
+
+        armor_index = item_ids[0] + " " + item_ids[1]
+        if (armor_index) in armor and item_ids[2] in material:
+            return [adjective[item_ids[3]], material[item_ids[2]], armor[armor_index], str(int(item_ids[-1]))]
 
     return False
