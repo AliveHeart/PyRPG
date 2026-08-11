794ecf6dfb3ad52c6443d38a351d286241294b6b
AliveHeart <mirarafathali7@gmail.com>
2026-08-11 19:11:23 +0530

    Support for local player

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import game
+import game, loader
 
-game.run()
+while True:
+    print("Enter save slot number")
+    plr_slot = input(">") or 1
+
+    data = loader.load_json("data/save_slots/" + str(plr_slot) + ".json")
+    if data:
+        break
+
+while True:
+    cmd = input(">") or "look"
+    game.game.run(cmd, plr_slot)
