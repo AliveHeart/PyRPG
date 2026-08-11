2734d4d80be0ed94555b7cf176be9357925c61c0
AliveHeart <mirarafathali7@gmail.com>
2026-08-11 19:11:46 +0530

    Add unbreaking layer

--- loader.py
+++ loader.py
@@ -1,12 +1,22 @@
 import json
+import os
 
 saves_path = "data/save_slots/"
 
 def load_json(path):
-    with open(path, "r") as f:
-        return json.load(f)
-    
+    try:
+        with open(path, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return False
+    except json.JSONDecodeError:
+        # Optional: handle case where file exists but is corrupted
+        return False
+
 def save_json(slot, data):
-    with open(saves_path + str(slot) + ".json", "w") as f:
-        json.dump(data, f, indent=4)
+    try:
+        with open(os.path.join(saves_path, f"{slot}.json"), "w") as f:
+            json.dump(data, f, indent=4)
         return True
+    except Exception:
+        return False
