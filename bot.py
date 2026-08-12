11624bbbb79f74207460d122be391315cffb9820
AliveHeart <mirarafathali7@gmail.com>
2026-08-12 17:45:44 +0530

    Update new commands

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from TokenAPI import Discord_Token
 import game, loader
 import os
 import discord
@@ -13,6 +12,13 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+def exe_cmd(command):
+    result = game.game.run(command, 1)#ctx.author.id)
+    new_text = ""
+    for text in result:
+        new_text += " " + text
+    return new_text
+
 @bot.event
 async def on_ready():
     print(f'✅ Bot is online as {bot.user}')
@@ -23,11 +29,39 @@ async def hello(ctx):
 
 @bot.command()
 async def look(ctx):
-    result = game.game.run("look", ctx.author.id)
-    new_text = ""
-    for text in result:
-        new_text += " " + text
-    await ctx.send(new_text)
+    await ctx.send(exe_cmd("look"))
+
+@bot.command()
+async def go(ctx, location: str):
+    await ctx.send(exe_cmd("go " + location))
+
+@bot.command()
+async def inv(ctx):
+    await ctx.send(exe_cmd("inventory"))
+
+@bot.command()
+async def fight(ctx, enemy:str):
+    await ctx.send(exe_cmd("fight " + enemy))
+
+@bot.command()
+async def attack(ctx):
+    await ctx.send(exe_cmd("attack"))
+
+@bot.command()
+async def defend(ctx):
+    await ctx.send(exe_cmd("defend"))
+
+@bot.command()
+async def run(ctx):
+    await ctx.send(exe_cmd("run"))
+
+@bot.command()
+async def kill(ctx):
+    await ctx.send(exe_cmd("kill"))
+
+@bot.command()
+async def spare(ctx):
+    await ctx.send(exe_cmd("spare"))
 
 
 bot.run(TOKEN)
