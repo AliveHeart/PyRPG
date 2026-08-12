e18d9a021abd7adf950da54a0e4cec16f803c6cc
AliveHeart <mirarafathali7@gmail.com>
2026-08-12 17:46:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ while True:
     data = loader.load_json("data/save_slots/" + str(plr_slot) + ".json")
     if data:
         break
+    else:
+        print("No save slot found!")
 
 while True:
     cmd = input(">") or "look"
-    game.game.run(cmd, plr_slot)
+    result = game.game.run(cmd, plr_slot)
+
+    for text in result:
+        print(text)
